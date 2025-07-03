b001ad66e1022cafe69482ead702dbf06f3f48ce
cjp0421 <76797183+cjp0421@users.noreply.github.com>
2025-07-03 09:40:21 -0500

    added basic repl logic anupdated gitignore

--- main.go
+++ main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
-	// cleanInput("hello world")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex >")
+		if !scanner.Scan() {
+			break
+		}
+		input := scanner.Text()
+		cleanedInput := cleanInput(string(input))
+		if len(cleanedInput) > 0 {
+			fmt.Printf("Your command was: %v\n", cleanedInput[0])
+		}
+	}
 }
 func cleanInput(text string) []string {
-	fmt.Println(text)
+	// fmt.Println(text)
 	lowerText := strings.ToLower(text)
 	cutset := " "
 	textStrings := (strings.Split(strings.Trim(lowerText, cutset), " "))
